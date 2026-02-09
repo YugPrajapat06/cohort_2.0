29ce9be2b53f74e4aee076493bc07e0c58042dcf
Yugvendra <yugprajapat6@gmail.com>
2026-02-09 10:22:47 +0530

    update git Ignore

---
+++ src/app.js
@@ -0,0 +1,59 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const noteModel = require("./model/notes.model")
+const path = require("path")
+const cors = require("cors")
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static("./public"))
+
+
+//Post /api/notes
+app.post("/api/notes", async (req, res) => {
+    const { title, description } = req.body
+    const note = await noteModel.create({
+        title, description
+    })
+    res.status(201).json({
+        message: "Note created successfully.",
+        note
+    })
+})
+
+//Get /api/notes
+app.get("/api/notes", async (req, res) => {
+    const notes = await noteModel.find()
+    res.status(200).json({
+        message: "Featched all data successfully.",
+        notes
+    })
+})
+
+//Delete /api/notes/:id
+app.delete("/api/notes/:id", async (req, res) => {
+    const id = req.params.id
+    await noteModel.findByIdAndDelete(id)
+    res.status(200).json({
+        message: "Note deleted successfully."
+    })
+})
+
+//Patch /api/notes/:id
+app.patch("/api/notes/:id", async (req, res) => {
+    const id = req.params.id
+    const { description } = req.body
+    await noteModel.findByIdAndUpdate(id, {
+        description
+    })
+    res.status(200).json({
+        message: "Note updated successfully."
+    })
+})
+
+app.use("*name",(req,res)=>{
+    res.sendFile(path.join(__dirname,"..","/public/index.html"))
+})
+module.exports = app
